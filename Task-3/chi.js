7884530edc24117da5bfbb5ba460ec277e0d0217
chiragsharma16 <chiragsharma1692000@gmail.com>
2023-06-04 19:49:01 +0530

    Task-3 Uploaded

---
+++ Task-3/chi.js
@@ -0,0 +1,16 @@
+function convert() {
+    const temperature = parseFloat(document.getElementById("temperature").value);
+    const unit = document.getElementById("unit").value;
+    let convertedTemperature;
+    let convertedUnit;
+  
+    if (unit === "celsius") {
+      convertedTemperature = temperature * 9 / 5 + 32;
+      convertedUnit = "Fahrenheit";
+    } else {
+      convertedTemperature = (temperature - 32) * 5 / 9;
+      convertedUnit = "Celsius";
+    }
+  
+    document.getElementById("result").innerHTML = `${convertedTemperature.toFixed(2)} ${convertedUnit}`;
+  }
